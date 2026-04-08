400fbd343ed6d39f6a9463dfccbe954d32805f26
khalil-mejri-1 <mejrik1888@gmail.com>
2026-04-08 20:15:43 +0100

    final update-9 4/1/2026

--- server/index.js
+++ server/index.js
@@ -26,6 +26,10 @@ app.get("/", (req, res) => {
     res.send("Server is running... 🚀");
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app;
